7e00c6dec598977f8e91421a99814edad00c6168
mateus <mateusdev23@gmail.com>
2025-07-03 20:02:33 -0300

    initial commit

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom';
+
+export default function SignUp() {
+
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    return(
+        <div className='container-center'>
+            <div className="login">
+                <div className="login-area">
+                    <img src={logo} alt="Logo do Sistema"/>
+                </div>
+
+                <form>
+                    <h1>Nova Conta</h1>
+                    <input type="text" placeholder='Seu nome' value={nome} onChange={(e) => setNome(e.target.value)}/>
+                    <input type="text" placeholder='email@email.com' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="password" placeholder='********' value={password} onChange={(e) => setPassword(e.target.value)}/>                    
+                
+                    <button type='submit'>Cadastrar</button>
+                
+                </form>
+
+
+                <Link to="/">Já possui uma conta? Faça login</Link>
+            </div>
+        </div>
+    )
+}
