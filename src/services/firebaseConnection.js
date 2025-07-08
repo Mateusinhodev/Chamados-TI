570d040d19db7d202166edc510182d548602cba1
mateus <mateusdev23@gmail.com>
2025-07-07 21:17:52 -0300

    Configurando o projeto Firebase

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJed3rFyPsy_ZwBwG1YhkImZGRXsd_Unc",
+  authDomain: "chamados-ti-7ac7a.firebaseapp.com",
+  projectId: "chamados-ti-7ac7a",
+  storageBucket: "chamados-ti-7ac7a.firebasestorage.app",
+  messagingSenderId: "196692540593",
+  appId: "1:196692540593:web:edf6254c3ce3f457e47690",
+  measurementId: "G-QJMMY6NPWN"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseConfig);
+const storage = getStorage(firebaseApp);
+
+export {auth, db, storage}
