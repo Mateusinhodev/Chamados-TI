f2bf7b4c554493f936f3e53330ffdc075d5ae25a
mateus <mateusdev23@gmail.com>
2025-07-17 21:52:21 -0300

    Privando rotas e logout

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -2,7 +2,7 @@ import { useState, createContext, useEffect} from 'react';
 // import SignIn from '../pages/SignIn';
 
 import {auth, db} from '../services/firebaseConnection';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { doc, getDoc, setDoc } from 'firebase/firestore'
 
 import { useNavigate } from 'react-router-dom';
@@ -16,8 +16,24 @@ function AuthProvider({children}) {
 
     const [user, setUser] = useState(null);
     const [loadingAuth, setLoadingAuth] = useState(false)
+    const [loading, setLoading] = useState(true);
 
     const navigate = useNavigate();
+
+    useEffect(() => {
+
+        async function  loadUser() {
+            const storageUser = localStorage.getItem('@ticketsPRO')
+
+            if(storageUser) {
+                setUser(JSON.parse(storageUser))
+                setLoading(false)
+            }
+
+            setLoading(false)
+        }
+        loadUser();
+    }, [])
 
     async function signIn(email, password) {
         setLoadingAuth(true);
@@ -87,6 +103,12 @@ function AuthProvider({children}) {
         localStorage.setItem('@ticketsPRO', JSON.stringify(data))
     }
 
+    async function logout() {
+        await signOut(auth);
+        localStorage.removeItem('@ticketsPRO');
+        setUser(null);
+    }
+
     return(
         <AuthContext.Provider 
             value={{
@@ -94,7 +116,9 @@ function AuthProvider({children}) {
                 user,
                 signIn,
                 signUp,
-                loadingAuth
+                logout,
+                loadingAuth,
+                loading
             }}
         >
             {children}
