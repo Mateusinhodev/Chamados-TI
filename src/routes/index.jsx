281f221539ee0b9b153efa627c5ed700d39378fa
mateus <mateusdev23@gmail.com>
2025-07-28 21:25:56 -0300

    Componente title

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,8 +3,10 @@ import { Routes, Route} from 'react-router-dom';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 import Private from './Private.jsx';
+
 
 function RoutesApp() {
     return(
@@ -13,6 +15,7 @@ function RoutesApp() {
             <Route path='/register' element={ <SignUp/> }/>
 
             <Route path='/dashboard' element={ <Private><Dashboard/></Private> }/>
+            <Route path='/profile' element={ <Private><Profile/></Private> }/>
         </Routes>
     )
 }
